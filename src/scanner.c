243aa5468f1238ba647f63225caeb3d670c11812
linewelder <redalerm@gmail.com>
2023-06-30 17:29:27 +0200

    Add whitespace skipping

--- src/scanner.c
+++ src/scanner.c
@@ -27,6 +27,10 @@ static char advance() {
     return scanner.current[-1];
 }
 
+static char peek() {
+    return *scanner.current;
+}
+
 static bool match(char expected) {
     if (isAtEnd()) return false;
     if (*scanner.current != expected) return false;
@@ -52,7 +56,27 @@ static Token errorToken(const char* message) {
     };
 }
 
+static void skipWhitespace() {
+    for (;;) {
+        char c = peek();
+        switch (c) {
+            case ' ':
+            case '\r':
+            case '\t':
+                advance();
+                break;
+            case '\n':
+                scanner.line++;
+                advance();
+                break;
+            default:
+                return;
+        }
+    }
+}
+
 Token scanToken() {
+    skipWhitespace();
     scanner.start = scanner.current;
 
     if (isAtEnd()) return makeToken(TOKEN_EOF);
