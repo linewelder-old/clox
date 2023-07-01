7e7e3eadaf164202e9ec97d7ce7c5ffa95983571
linewelder <redalerm@gmail.com>
2023-07-01 13:04:41 +0200

    Add string value type

--- src/object.h
+++ src/object.h
@@ -6,6 +6,11 @@
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
+
+#define AS_STRING(value) ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value) (AS_STRING(value)->chars)
+
 typedef enum {
     OBJ_STRING,
 } ObjType;
@@ -13,5 +18,15 @@ typedef enum {
 struct Obj {
     ObjType type;
 };
+
+struct ObjString {
+    Obj obj;
+    int length;
+    char* chars;
+};
+
+static inline bool isObjType(Value value, ObjType type) {
+    return IS_OBJ(value) && OBJ_TYPE(value) == type;
+}
 
 #endif
