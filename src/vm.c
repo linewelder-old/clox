linewelder-old/clox

commit c0aa385ff983d040e8250e2d2fe099b9d7d98205

Optimize arithmetic operations

src/vm.c
@@ -49,9 +49,10 @@ static InterpretResult run() {
     ])
 #define BINARY_OP(op) \
     do { \
-        double b = pop(); \
-        double a = pop(); \
-        push(a op b); \
+        vm.stackTop--; \
+        double b = vm.stackTop[1]; \
+        double a = vm.stackTop[0]; \
+        *vm.stackTop = a op b; \
     } while (false)
 
     for (;;) {
@@ -82,7 +83,7 @@ static InterpretResult run() {
             case OP_SUBTRACT: BINARY_OP(-); break;
             case OP_MULTIPLY: BINARY_OP(*); break;
             case OP_DIVIDE:   BINARY_OP(/); break;
-            case OP_NEGATE:   push(-pop()); break;
+            case OP_NEGATE:   *vm.stackTop = -*vm.stackTop; break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");

commit c91ec4d75f2e1f0cfb75683863995a226d5008c0

Fix bug caused by c0aa385

src/vm.c
@@ -51,9 +51,9 @@ static InterpretResult run() {
 #define BINARY_OP(op) \
     do { \
         vm.stackTop--; \
-        double b = vm.stackTop[1]; \
-        double a = vm.stackTop[0]; \
-        *vm.stackTop = a op b; \
+        double b = vm.stackTop[0]; \
+        double a = vm.stackTop[-1]; \
+        vm.stackTop[-1] = a op b; \
     } while (false)
 
     for (;;) {
@@ -84,7 +84,9 @@ static InterpretResult run() {
             case OP_SUBTRACT: BINARY_OP(-); break;
             case OP_MULTIPLY: BINARY_OP(*); break;
             case OP_DIVIDE:   BINARY_OP(/); break;
-            case OP_NEGATE:   *vm.stackTop = -*vm.stackTop; break;
+            case OP_NEGATE:
+                vm.stackTop[-1] = -vm.stackTop[-1];
+                break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");