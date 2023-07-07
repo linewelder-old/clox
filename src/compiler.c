03423a0988fea43699b40fc726ded5d7896cb4b6
linewelder <redalerm@gmail.com>
2023-07-07 21:04:55 +0200

    Make referencing local in its initializer error

--- src/compiler.c
+++ src/compiler.c
@@ -217,6 +217,9 @@ static int resolveLocal(Compiler* compiler, Token* name) {
     for (int i = compiler->localCount - 1; i >= 0; i--) {
         Local* local = &compiler->locals[i];
         if (identifiersEqual(name, &local->name)) {
+            if (local->depth == -1) {
+                error("Can't read local variable in its own initializer.");
+            }
             return i;
         }
     }
@@ -232,7 +235,7 @@ static void addLocal(Token name) {
 
     Local* local = &current->locals[current->localCount++];
     local->name = name;
-    local->depth = current->scopeDepth;
+    local->depth = -1;
 }
 
 static void declareVariable() {
@@ -387,8 +390,13 @@ static uint8_t parseVariable(const char* errorMessage) {
     return identifierConstant(&parser.previous);
 }
 
+static void markInitialized() {
+    current->locals[current->localCount - 1].depth = current->scopeDepth;
+}
+
 static void defineVariable(uint8_t global) {
     if (current->scopeDepth > 0) {
+        markInitialized();
         return;
     }
 
