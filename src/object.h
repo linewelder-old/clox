fa0cd0b287a3184f343e062799b9fca7c12c5a11
linewelder <redalerm@gmail.com>
2023-07-21 15:41:01 +0200

    Add argument number checking for native functions

--- src/object.h
+++ src/object.h
@@ -12,8 +12,7 @@
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 
 #define AS_FUNCTION(value) ((ObjFunction*)AS_OBJ(value))
-#define AS_NATIVE(value) \
-    (((ObjNative*)AS_OBJ(value))->function)
+#define AS_NATIVE(value) ((ObjNative*)AS_OBJ(value))
 #define AS_STRING(value) ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value) (AS_STRING(value)->chars)
 
@@ -35,10 +34,11 @@ typedef struct {
     ObjString* name;
 } ObjFunction;
 
-typedef Value (*NativeFn)(int argCount, Value* args);
+typedef Value (*NativeFn)(Value* args);
 
 typedef struct {
     Obj obj;
+    int arity;
     NativeFn function;
 } ObjNative;
 
@@ -50,7 +50,7 @@ struct ObjString {
 };
 
 ObjFunction* newFunction();
-ObjNative* newNative(NativeFn function);
+ObjNative* newNative(NativeFn function, int arity);
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
 void printObject(Value value);
