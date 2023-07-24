2d4079a1a1f6ed5ef156c9f3dfce8c6f8f6829cd
linewelder <redalerm@gmail.com>
2023-07-24 15:42:25 +0200

    Add recursive upvalue resolution

--- src/compiler.c
+++ src/compiler.c
@@ -336,6 +336,11 @@ static int resolveUpvalue(Compiler* compiler, Token* name) {
         return addUpvalue(compiler, (uint8_t)local, true);
     }
 
+    int upvalue = resolveUpvalue(compiler->enclosing, name);
+    if (upvalue != -1) {
+        return addUpvalue(compiler, (uint8_t)upvalue, false);
+    }
+
     return -1;
 }
 
