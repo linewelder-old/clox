d9cdc9eeeb338767ef5afd65421c28cb151b0dbd
linewelder <redalerm@gmail.com>
2023-08-14 07:30:37 +0200

    Add stack for "gray" objects

--- src/memory.c
+++ src/memory.c
@@ -34,6 +34,16 @@ void markObject(Obj* object) {
     printf("\n");
 #endif
     object->isMarked = true;
+
+    if (vm.grayCapacity < vm.grayCount + 1) {
+        vm.grayCapacity = GROW_CAPACITY(vm.grayCapacity);
+        vm.grayStack = (Obj**)realloc(vm.grayStack,
+                                      sizeof(Obj*) * vm.grayCapacity);
+
+        if (vm.grayStack == NULL) exit(1);
+    }
+
+    vm.grayStack[vm.grayCount++] = object;
 }
 
 void markValue(Value value) {
@@ -112,4 +122,6 @@ void freeObjects() {
         freeObject(object);
         object = next;
     }
+
+    free(vm.grayStack);
 }
